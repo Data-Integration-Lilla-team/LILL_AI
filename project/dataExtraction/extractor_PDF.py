fb8d440c063beb4968e3490033b585e40bcdc4c3
paolo-di-simone <72343829+paolo-di-simone@users.noreply.github.com>
2023-06-13 11:02:12 +0200

    paolo

--- project/dataExtraction/extractor_PDF.py
+++ project/dataExtraction/extractor_PDF.py
@@ -1,24 +1,22 @@
 from PyPDF2 import PdfReader
 
 class PDF_Text_Extractor:
+
     def __init__(self):
         pass
+
 
     def extract_data(self, list_of_path):
         print("Estrazione dati pdf")
         print(list_of_path)
-        dict_path_dict_page_text = {}
+        dict_path_page_text = {}
         for file in list_of_path:
             print(file)
-            dict_page_text = {}
             reader = PdfReader(file)
             n_pages = len(reader.pages)
             for i in range(n_pages):
                 page = reader.pages[i]
                 text = page.extract_text()
-                key=file+"#"+str(i)
-                dict_path_dict_page_text[key]=text
-                #old version of saving pages
-                #dict_page_text[str(i)] = text
-            #dict_path_dict_page_text[file] = dict_page_text
-        return dict_path_dict_page_text
+                key = file + "#" + str(i)
+                dict_path_page_text[key] = text
+        return dict_path_page_text
--- project/dataExtraction/extractor_pptx.py
+++ project/dataExtraction/extractor_pptx.py
@@ -1,20 +1,22 @@
 from pptx import Presentation
 
 class PPTX_Text_Extractor:
+
     def __init__(self):
         pass
     
+
     def sort_dic(self,dic):
-            
         sorted_dict = {}
         for key in sorted(dic.keys()):
             sorted_dict[key] = dic[key]
         return sorted_dict
+
+
     def extract_data(self, list_of_path):
         print("Estrazione dati pptx")
-        dict_path_dict_page_text = {}
+        dict_path_page_text = {}
         for file in list_of_path:
-            dict_page_text = {}
             presentation = Presentation(file)
             for idx, slide in enumerate(presentation.slides):
                 print(idx)
@@ -22,11 +24,6 @@ class PPTX_Text_Extractor:
                 for shape in slide.shapes:
                     if hasattr(shape, "text"):
                         text += shape.text
-                    key=file+"#"+str(idx)
-                    dict_path_dict_page_text[key]=text
-                #old version
-                    #dict_page_text[idx] = text
-                #dict_page_text=self.sort_dic(dict_page_text)
-
-            dict_path_dict_page_text[file] = dict_page_text
-        return dict_path_dict_page_text
+                key = file + "#" + str(idx)
+                dict_path_page_text[key] = text
+        return dict_path_page_text
